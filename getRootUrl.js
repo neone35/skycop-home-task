ba7696beab4ff7e7bf100138e5c42115b127c38c
Artur Maslov <aartasas@gmail.com>
2018-05-08 17:32:33 +0300

    Finnish styling and add travis config to push to heroku

--- getRootUrl.js
+++ getRootUrl.js
@@ -2,7 +2,7 @@ export default function getRootUrl() {
   const port = process.env.PORT || 3000;
   const dev = process.env.NODE_ENV !== 'production';
   // change production to https://rearn.org
-  const ROOT_URL = dev ? `http://localhost:${port}` : 'https://some-domain.com';
+  const ROOT_URL = dev ? `http://localhost:${port}` : 'https://skycop-task.herokuapp.com';
 
   return ROOT_URL;
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -35,6 +35,7 @@ class Index extends React.Component {
           label: dropdownTitles[index],
           value: dropdownIds[index],
         }));
+      // console.log(options);
       dropdown = (
         <Dropdown
           options={options}
@@ -52,19 +53,27 @@ class Index extends React.Component {
 
   render() {
     const { value } = this.state.selected;
-    console.log(value);
     // const defaultOption = payload[0].title;
     return (
       <div className="formWrapper">
-        <h2>Skycop dropdown</h2>
-        <button
-          className="buttonStyle"
-          onClick={this.props.fetchSkycopResponse}
-        >Fetch data
-        </button>
-        {this.renderDropdown()}
-        <br />
-        <p>You selected id# <br /> {value}</p>
+        <div className="jumbotron">
+          <h1 className="display-4">Skycop dropdown</h1>
+          <p className="lead" style={{ fontSize: '1em' }}>You have to create a simple React/React-Redux App that `fetches` a response into Redux state, displays a dropdown with the results, and upon changing a selection, it`s value (not title) has to be displayed somewhere.</p>
+          <hr className="my-4" />
+          <div className="lead">
+            <p>
+              You selected id# <br />
+              <span style={{ color: '#33BB6C' }} >{value}</span>
+            </p>
+            {this.renderDropdown()}
+            <button
+              className="buttonStyle btn btn-outline-primary"
+              onClick={this.props.fetchSkycopResponse}
+            >
+              Fetch data
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
